ecdb2e1c2ca058720e8ac5c0adc9039ba1ba6ac7
Roman Brito <romanbrito1@gmail.com>
2017-07-07 16:34:26 -0500

    template based subscriptions, showing products in home

--- client/templates/category/category.js
+++ client/templates/category/category.js
@@ -1,9 +1,15 @@
+//template based subscriptions product should be available to category template
+Template.category.onCreated(function () {
+  this.subscribe('products');
+});
 Template.category.helpers({
     categoryName:function(){
         return FlowRouter.getParam('categoryName');
-    }
+    },
+  products:function () {
+    return Product.find();
+  }
 });
-
 Template.categoryAdmin.events({
     'click .addCategory':function(event,template){
        var category = {};
