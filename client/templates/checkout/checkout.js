a76c2ab7ce6cf6e60266a018e58e2354ddfe16b5
Roman Brito <romanbrito1@gmail.com>
2017-07-08 04:40:12 -0500

    some images

--- client/templates/checkout/checkout.js
+++ client/templates/checkout/checkout.js
@@ -9,15 +9,16 @@ Template.checkout.helpers({
 Template.paypalCreditCardForm.events({
     'submit #paypal-payment-form':function(evt,tmpl){
         evt.preventDefault();
-        if(Number(Session.get('cartTotal')) > 0) {
-            var cardData = Template.paypalCreditCardForm.card_data();
-            Meteor.Paypal.purchase(cardData, {total: Session.get('cartTotal'), currency: 'USD'}, function (err, results) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log(results)
-                }
-            })
-        }
+        console.log("paypal submited add points");
+        // if(Number(Session.get('cartTotal')) > 0) {
+        //     var cardData = Template.paypalCreditCardForm.card_data();
+        //     Meteor.Paypal.purchase(cardData, {total: Session.get('cartTotal'), currency: 'USD'}, function (err, results) {
+        //         if (err) {
+        //             console.log(err);
+        //         } else {
+        //             console.log(results)
+        //         }
+        //     })
+        // }
     }
-})
+});
